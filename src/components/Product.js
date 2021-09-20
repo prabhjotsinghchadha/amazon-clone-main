8823cb97031145aa10386afe2a7d9612c76feecc
prabhjotsinghchadha <prabhjottechs@gmail.com>
2021-09-20 20:01:01 +0530

    Currency INR added

--- src/components/Product.js
+++ src/components/Product.js
@@ -58,7 +58,7 @@ function Product({ id, title, price, description, category, image }) {
       <p className="text-xs my-2 line-clamp-2">{description}</p>
 
       <div className="mb-5">
-        <Currency quantity={price} currency="GBP" />
+        <Currency quantity={price * 101} currency="INR" />
       </div>
 
       {hasPrime && (
